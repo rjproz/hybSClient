481d6903bd74262c3f9c1d7d9f008eba971d5d38
Rajkumar Pramanik "RJproz <rjproz@gmail.com>
2023-06-10 03:16:50 +0530

    - Persistent World Test

---
+++ Assets/_PersistentWorldTest/Scripts/NetworkManager.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using System.Collections.Generic;
+using LiteNetLib;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour,ILNSManagerDataReceiver
+{
+    public bool isServer { get; set; } = false;
+
+    public GameObject treePrefab;
+    public Transform plantHud;
+    private Dictionary<string, TreeNetwork> trees = new Dictionary<string, TreeNetwork>();
+
+    
+    public void Awake()
+    {
+        treePrefab.SetActive(false);
+
+#if UNITY_SERVER 
+        isServer = true;
+        Application.targetFrameRate = 10;
+#endif
+    }
+
+
+     
+    [ContextMenu("Plant At Hud")]
+    public void PlantAtHUD()
+    {
+        PlantTreeAt(plantHud.position);
+        plantHud.position += Vector3.forward;
+    }
+
+    public void PlantTreeAt(Vector3 pos)
+    {
+        LNSWriter writer = LNSWriter.GetFromPool();
+        writer.Reset();
+        writer.Put(pos);
+        LNSManager.RaiseEventOnMasterClient(12, writer, DeliveryMethod.ReliableOrdered);
+        writer.Recycle();
+    }
+
+
+    void ILNSManagerDataReceiver.OnCachedDataReceived(string ofKey, byte[] rawData)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    void ILNSManagerDataReceiver.OnEventRaised(LNSClient from, ushort eventCode, LNSReader reader, DeliveryMethod deliveryMethod)
+    {
+        //throw new System.NotImplementedException();
+        
+        if(eventCode == 12)
+        {
+            if (isServer)
+            {
+                //plant
+                
+                var pos = reader.GetVector3();
+
+                var tree = Instantiate(treePrefab);
+                tree.transform.position = pos;
+                tree.SetActive(true);
+
+                var treeScript = tree.GetComponent<TreeNetwork>();
+                treeScript.id = LNSManager.GenerateNextId();
+                treeScript.StartGrow();
+
+
+                trees.Add(treeScript.id, treeScript);
+            }
+
+        }
+        else if (eventCode == 13)
+        {
+            if (isServer)
+            {
+                //delete
+                var id = reader.GetString();
+                var ga = trees[id];
+                trees.Remove(id);
+                Destroy(ga.gameObject);
+                Resources.UnloadUnusedAssets();
+            }
+
+        }
+        else if(eventCode == 11)
+        {
+            var id = reader.GetString();
+            var pos = reader.GetVector3();
+            var scale = reader.GetVector3();
+
+            TreeNetwork treeNetwork = null;
+            if(trees.ContainsKey(id))
+            {
+                treeNetwork = trees[id];
+            }
+            else
+            {
+                var tree = Instantiate(treePrefab);
+                tree.transform.position = pos;
+                tree.SetActive(true);
+
+                var treeScript = tree.GetComponent<TreeNetwork>();
+                treeScript.id = id;
+                trees.Add(treeScript.id, treeScript);
+                treeNetwork = treeScript;
+
+            }
+            treeNetwork.transform.position = pos;
+            treeNetwork.transform.localScale = scale;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LNSClientParameters clientParameters = new LNSClientParameters(SystemInfo.deviceUniqueIdentifier + Application.isEditor);
+        LNSConnectSettings connectSettings = new LNSConnectSettings();
+       
+        connectSettings.serverIp = "45.55.33.88";
+        connectSettings.serverPort = 10002;
+        connectSettings.serverSecurityKey = ServerKey.GetKey();
+        connectSettings.gameKey = "hybriona.persistent_world";
+
+        LNSManager.Initialize(clientParameters, connectSettings, this);
+
+
+        System.Action roomAction = () =>
+        {
+            Debug.Log("Room joined");
+            if (isServer)
+            {
+                Debug.Log("I am server");
+                LNSManager.connector.MakeMeMasterClient();
+                //for (int i = 0; i < 10; i++)
+                //{
+                //    var tree = Instantiate(treePrefab);
+                //    tree.transform.position = new Vector3(Random.Range(-40, 40), 0, Random.Range(-40, 40));
+                //    tree.SetActive(true);
+
+                //    var treeScript = tree.GetComponent<TreeNetwork>();
+                //    treeScript.id = LNSManager.GenerateNextId();
+                //    treeScript.StartGrow();
+
+
+                //    trees.Add(treeScript.id, treeScript);
+
+
+                //}
+            }
+        };
+        LNSManager.connector.onRoomCreated = ()=> roomAction();
+        LNSManager.connector.onRoomJoined = ()=> roomAction();
+        LNSManager.connector.onConnected = () =>
+        {
+            Debug.Log("Connected");
+
+            LNSManager.connector.JoinRoomOrCreateIfNotExist("default");
+        };
+
+        LNSManager.Connect();
+    }
+
+   
+}
---
+++ Assets/_PersistentWorldTest/Scripts/TreeNetwork.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeNetwork : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float growthRate = 0;
+    public float maxHeight = 10;
+    public float maxWidth = 5;
+
+    public string id;
+   
+
+    private bool canGrow = false;
+    private LNSWriter commonWriter;
+    public void StartGrow()
+    {
+        commonWriter = new LNSWriter();
+        canGrow = true;
+
+        StartCoroutine(SendData());
+    }
+
+    IEnumerator SendData()
+    {
+        WaitForSeconds wait = new WaitForSeconds(1);
+        while(true)
+        {
+            if(LNSManager.connector.isConnected)
+            {
+                commonWriter.Reset();
+                commonWriter.Put(id);
+                commonWriter.Put(transform.position);
+                commonWriter.Put(transform.localScale);
+                LNSManager.RaiseEventOnAll(11, commonWriter, LiteNetLib.DeliveryMethod.ReliableSequenced);
+                
+            }
+            yield return wait;
+        }
+    }
+
+    public void Update()
+    {
+       
+        if (canGrow)
+        {
+
+            var currentHeight = transform.localScale.y;
+
+            if (currentHeight < maxHeight)
+            {
+                float newHeight = currentHeight + Time.deltaTime * growthRate;
+                float newWidth = Mathf.Lerp(1, maxWidth, newHeight / maxHeight);
+                transform.localScale = new Vector3(newWidth, newHeight, newWidth);
+            }
+        }
+    }
+}
