c535f99e26d8c5f8f171b6c18ac7b5e5ac217109
Rajkumar Pramanik "RJproz <rjproz@gmail.com>
2024-06-08 16:17:13 +0530

    no message

--- Assets/Hybriona/LNSClient/LNSHighLevel/Scripts/LNSInterpolation.cs
+++ Assets/Hybriona/LNSClient/LNSHighLevel/Scripts/LNSInterpolation.cs
@@ -8,18 +8,14 @@ public class LNSInterpolation
 
     public static InterpolationVariable CalcuateInterpolationVariables(Vector3 from, Vector3 to, float time)
     {
-        InterpolationVariable o = new InterpolationVariable();
-        o.from = from;
-        o.to = to;
-        o.timeLength = time;
-  
-        o.velocity = (to - from) / time;
-        o.timer = 0;
+        InterpolationVariable o = new InterpolationVariable(from,to,time,(to-from)/time);
+       
         return o;
     }
-    public static Vector3 InterpolateVector3(Vector3 from, Vector3 to, float speed)
+
+    public static Vector3 InterpolateVector3(InterpolationVariable variable)
     {
-        return from + (to - from) * speed;
+        return variable.from + (variable.to - variable.from) * Mathf.Clamp01( variable.timer /variable.timeLength);
     }
 
     public static Vector3 InterpolateEulers(Vector3 from, Vector3 to)
@@ -31,9 +27,38 @@ public class LNSInterpolation
 [System.Serializable]
 public struct InterpolationVariable
 {
-    public Vector3 from;
-    public Vector3 to;
-    public float timeLength;
-    public Vector3 velocity;
-    public float timer;
+    public Vector3 from { get; private set; }
+    public Vector3 to { get; private set; }
+    public float timeLength { get; private set; }
+    public Vector3 velocity { get; private set; }
+    public float speed { get; private set; }
+    public bool hasValue { get; private set; }
+
+    public float timer { get; private set; }
+
+    public InterpolationVariable(Vector3 from,Vector3 to,float timeLength,Vector3 velocity)
+    {
+        this.hasValue = true;
+        this.from = from;
+        this.to = to;
+        this.timeLength = timeLength;
+        this.velocity = velocity;
+        this.speed = velocity.magnitude;
+        this.timer = 0;
+    }
+
+    public void ResetTimer()
+    {
+        this.timer = 0;
+    }
+
+    public void UpdateTimer(float delta)
+    {
+        this.timer += delta;
+    }
+
+    public void UpdateTimer()
+    {
+        this.timer += Time.deltaTime;
+    }
 }
---
+++ Assets/_Samples/InterpolationTest/InterpolationTest.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class InterpolationTest : MonoBehaviour
+{
+    public Transform mainPlayer;
+    public float mainPlayerSpeed = 1;
+
+    public Transform clonePlayer;
+    public float sendRate = 10;
+    public int latency_ms = 50;
+    [SerializeField]
+    Vector3 targetPoint;
+
+    private InterpolationController interpolationController;
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        interpolationController = new InterpolationController();
+        interpolationController.AssignTransform(clonePlayer);
+        StartCoroutine(SendUpdateToClonePlaye());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out RaycastHit hit))
+            {
+                targetPoint = hit.point;
+            }
+        }
+    }
+
+    IEnumerator SendUpdateToClonePlaye()
+    {
+        while(true)
+        {
+            UpdateClonePlayer(mainPlayer.position, Time.time);
+            float timer = 1f / sendRate;
+            while(timer > 0)
+            {
+                timer -= Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+
+   
+
+    private void LateUpdate()
+    {
+        mainPlayer.position = Vector3.MoveTowards(mainPlayer.position, targetPoint, mainPlayerSpeed * Time.deltaTime);
+        interpolationController.Interpolation();
+    }
+
+
+
+   
+    private async void UpdateClonePlayer(Vector3 position, float time)
+    {
+        await Task.Delay(latency_ms);
+        interpolationController.Update(position, time);
+       
+       
+    }
+
+
+}
+
+public class InterpolationController
+{
+    Transform transform;
+    private float lastRecordedTime = -1;
+
+    public void AssignTransform(Transform transform)
+    {
+        this.transform = transform;
+    }
+
+    InterpolationVariable parameter;
+    
+    public void Update(Vector3 position, float time)
+    {
+        
+        parameter = LNSInterpolation.CalcuateInterpolationVariables(transform.position, position, time - lastRecordedTime);
+        lastRecordedTime = time;
+    }
+
+    public void Interpolation()
+    {
+        //LNSInterpolation.InterpolateVector3(parameter.from,);
+        if (parameter.hasValue)
+        {
+            transform.position = LNSInterpolation.InterpolateVector3(parameter);
+            parameter.UpdateTimer(Time.deltaTime);
+
+            //transform.position = Vector3.MoveTowards(transform.position, parameter.to, Time.deltaTime * parameter.speed);
+        }
+        
+        
+    }
+}
+
+
